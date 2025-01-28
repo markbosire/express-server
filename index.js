18bedc01e767dcf529836170a27b9f483449bfc0
mark bosire <markbosirekenyariri@gmail.com>
2025-01-28 19:01:17 +0300

    added kubernetes config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, its me World!');
+  res.send('Hello, its me mario World!');
 });
 
 app.listen(port, () => {
